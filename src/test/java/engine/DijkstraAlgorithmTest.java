0a38a6305e8352a0bbfbf307c8c21bce951b634b
Daria Kalashnikova <dashavinni0@gmail.com>
2018-03-04 21:01:46 +0100

    modifying the unit test

--- src/test/java/engine/DijkstraAlgorithmTest.java
+++ src/test/java/engine/DijkstraAlgorithmTest.java
@@ -2,17 +2,22 @@ package engine;
 
 import interfaces.Edge;
 import interfaces.Vertex;
-import models.*;
+import models.City;
+import models.Flight;
+import models.Graph;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class DijkstraAlgorithmTest {
+
     private List<Vertex> nodes;
     private List<Edge> edges;
 
@@ -20,41 +25,53 @@ public class DijkstraAlgorithmTest {
     public void testExecute() {
         nodes = new ArrayList<>();
         edges = new ArrayList<>();
-        for (int i = 0; i < 11; i++) {
-            Vertex location = new City("Node_" + i, "Node_" + i, i);
-            nodes.add(location);
-        }
 
-        addLane("Edge_0", 0, 1, 85);
-        addLane("Edge_1", 0, 2, 217);
-        addLane("Edge_2", 0, 4, 173);
-        addLane("Edge_3", 2, 6, 186);
-        addLane("Edge_4", 2, 7, 103);
-        addLane("Edge_5", 3, 7, 183);
-        addLane("Edge_6", 5, 8, 250);
-        addLane("Edge_7", 8, 9, 84);
-        addLane("Edge_8", 7, 9, 167);
-        addLane("Edge_9", 4, 9, 502);
-        addLane("Edge_10", 9, 10, 40);
-        addLane("Edge_11", 1, 10, 600);
+        Vertex locations[] = new Vertex[]{
+                new City("A", "A", 0),
+                new City("B", "B", 0),
+                new City("C", "C", 0),
+                new City("D", "D", 0),
+                new City("E", "E", 0)};
 
-        // Lets check from location Loc_1 to Loc_10
+        nodes.addAll(Arrays.asList(locations));
+
+        addLane("A->D", 0, 3, 1);
+        addLane("A->B", 0, 1, 6);
+        addLane("D->B", 3, 1, 2);
+        addLane("B->E", 1, 4, 2);
+        addLane("D->E", 3, 4, 1);
+        addLane("E->C", 4, 2, 5);
+        addLane("B->C", 1, 2, 5);
+
+        // Lets check from locations Loc_1 to Loc_10
         Graph graph = new Graph(nodes, edges);
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
         dijkstra.execute(nodes.get(0));
-        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(10));
+        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(2));
 
         assertNotNull(path);
         assertTrue(path.size() > 0);
 
-        for (Vertex vertex : path) {
-            System.out.println(vertex);
-        }
-
+        printResult(path);
     }
 
     private void addLane(String laneId, int sourceLocNo, int destLocNo, int duration) {
         Edge lane = new Flight(laneId, (City) nodes.get(sourceLocNo), (City) nodes.get(destLocNo), Duration.ofMinutes(duration), duration, "DummyLine");
         edges.add(lane);
+    }
+
+    private void printResult(List<Vertex> path) {
+        int i = 0;
+        System.out.println();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Vertex vertex : path) {
+            if (i++ == path.size() - 1) {
+                stringBuilder.append(vertex.getName());
+            } else {
+                stringBuilder.append(vertex.getName()).append("->");
+            }
+        }
+
+        System.out.println("The shortest path: " + stringBuilder);
     }
 }
